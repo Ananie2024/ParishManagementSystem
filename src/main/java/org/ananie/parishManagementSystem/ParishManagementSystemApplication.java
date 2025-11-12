b9149a9bf9098b40c6d46184d0d0171c3b6e9f44
Ananie2024 <anahafasha@gmail.com>
2025-11-12 19:28:17 +0200

    Initialize new Spring Boot project

---
+++ src/main/java/org/ananie/parishManagementSystem/ParishManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package org.ananie.parishManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParishManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParishManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ananie/parishManagementSystem/ParishManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ananie.parishManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParishManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
